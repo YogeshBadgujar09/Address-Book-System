d4a3f27077348df1d364f01aa75b2e81e5b823a8
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-03-16 23:06:23 +0530

    [MODIFIED] : Yogesh - Create Contacts In ContactsModule Class

---
+++ src/com/yogesh/addressbook/ContactsModule.java
@@ -0,0 +1,13 @@
+package com.yogesh.addressbook;
+
+public class ContactsModule {
+
+    private String fName ;
+    private String lName;
+    private String address ;
+    private String city ;
+    private String state;
+    private String zipCode;
+    private String phoneNo;
+    private String email ;
+}
