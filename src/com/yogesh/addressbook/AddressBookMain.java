5926efc9064d710866b1836edb9de362d924e063
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-03-16 23:49:58 +0530

    [MODIFIED] : Yogesh - add a new contact in Book

--- src/com/yogesh/addressbook/AddressBookMain.java
+++ src/com/yogesh/addressbook/AddressBookMain.java
@@ -1,10 +1,51 @@
 package com.yogesh.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+
+    ContactsModule contactsModule = new ContactsModule();
+    Scanner scanner = new Scanner(System.in);
+
+    public void createContact(){
+
+        System.out.println("Enter First Name :");
+        contactsModule.setfName(scanner.next());
+
+        System.out.println("Enter Last Name :");
+        contactsModule.setlName(scanner.next());
+
+        System.out.println("Enter Address :");
+        contactsModule.setAddress(scanner.next());
+
+        System.out.println("Enter City :");
+        contactsModule.setCity(scanner.next());
+
+        System.out.println("Enter State :");
+        contactsModule.setState(scanner.next());
+
+        System.out.println("Enter Zip code :");
+        contactsModule.setZipCode(scanner.next());
+
+        System.out.println("Enter Phone no. :");
+        contactsModule.setPhoneNo(scanner.next());
+
+        System.out.println("Enter Email :");
+        contactsModule.setEmail(scanner.next());
+
+    }
+
+    void displayContact(){
+        System.out.println(" " + contactsModule.toString());
+    }
 
     public static void main(String[] args){
 
         System.out.println("Welcome To Address Book");
+
+        AddressBookMain addressBookMain = new AddressBookMain();
+        addressBookMain.createContact();
+        addressBookMain.displayContact();
 
     }
 }
--- src/com/yogesh/addressbook/ContactsModule.java
+++ src/com/yogesh/addressbook/ContactsModule.java
@@ -1,5 +1,7 @@
 package com.yogesh.addressbook;
 
+import com.sun.org.apache.bcel.internal.generic.ARETURN;
+
 public class ContactsModule {
 
     private String fName ;
@@ -10,4 +12,82 @@ public class ContactsModule {
     private String zipCode;
     private String phoneNo;
     private String email ;
+
+    public void setfName(String fName){
+        this.fName = fName ;
+    }
+
+    public void setlName(String lName){
+        this.lName = lName;
+    }
+
+    public void setAddress(String address){
+        this.address = address ;
+    }
+
+    public void setCity(String city){
+        this.city = city;
+    }
+
+    public void setState(String state){
+        this.state = state ;
+    }
+
+    public void setZipCode(String zipCode){
+        this.zipCode = zipCode ;
+    }
+
+    public void setPhoneNo(String phoneNo){
+        this.phoneNo = phoneNo ;
+    }
+
+    public void setEmail(String email){
+        this.email = email ;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactsModule{" +
+                "fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", phoneNo='" + phoneNo + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
