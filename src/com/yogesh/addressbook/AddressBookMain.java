9078e5c86dd1226310ba43700bd9bc5622656961
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-05-09 00:45:47 +0530

    [MODIFIED] : Yogesh - Contact Edit Functionatity work properly

--- src/com/yogesh/addressbook/AddressBookMain.java
+++ src/com/yogesh/addressbook/AddressBookMain.java
@@ -73,9 +73,10 @@ public class AddressBookMain {
     {
         for(int i=0 ; i<contactsList.size() ; i++)
          {
+
              ContactsModel contactsModel = contactsList.get(i);
 
-             System.out.println(contactsModel.toString());
+             System.out.println("\n" + contactsModel.toString());
 
          }
     }
@@ -98,6 +99,7 @@ public class AddressBookMain {
             {
                 flag = true;
                 System.out.println("Given name is Found in Contact");
+                break;
             }
         }
 
@@ -126,45 +128,37 @@ public class AddressBookMain {
 
             System.out.println("Enter Email :");
             contactsModel.setEmail(scanner.next());
+
         }
 
     }
 
-    /*
-        if(fname.equals(contactsModel.getFirstName()))
-        {
-            System.out.println("Contact Found.");
-
-
-        }
-        else
-        {
-            System.out.println("Contact Not Found.");
-        }
-
-
-
-
-     */
-
-    /*
-    public void deleteContacts()
+   /* public void deleteContacts()
     {
         System.out.println("Enter First Name To Delete :");
         String fname = scanner.next();
 
-        if(fname.equals(contactsModel.getFirstName()))
+        boolean flag = false ;
+
+        for(int i=0 ; i<contactsList.size() ; i++)
         {
-            contactsModel = null ;
-            System.out.println("Contact is Delete");
+            ContactsModel contactsModel = contactsList.get(i);
+
+            if(fname.equals(contactsModel.getFirstName()))
+            {
+                flag = true ;
+                System.out.println("Contact is Delete");
+            }
+
         }
-        else{
+
+        if(!flag){
+
             System.out.println("Contact is not found .");
         }
     }
+*/
 
-
-    */
 
     public static void main(String[] args){
 
